59c8cd34829f76416d0903f160355d85f874dcff
mike <m.mende@live.de>
2019-12-10 14:48:24 +0100

    Added annotation for Controller

--- src/main/java/com/example/sidecar_postgres/controller/AthleteController.java
+++ src/main/java/com/example/sidecar_postgres/controller/AthleteController.java
@@ -9,15 +9,19 @@ import com.example.sidecar_postgres.entity.Athlete;
 import com.example.sidecar_postgres.repository.AthleteRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author mike
  */
+@RestController
+@Transactional
 public class AthleteController {
         
     @Autowired
